cc8d8a8df34ce3951f340bce97a06a7e4d600496
Henrique Mafra <henriquemafra89@gmail.com>
2023-06-14 11:37:37 -0300

    Adiciona exercicio Objeto Aluno

---
+++ objetoAluno.js
@@ -0,0 +1,6 @@
+let aluno = {}
+
+aluno.nome = prompt("Digite o nome do aluno: ")
+aluno.idade = parseInt(prompt("Digite a idade do aluno: "))
+
+alert("Nome do aluno: " + aluno.nome + "\nIdade do aluno: " + aluno.idade)
